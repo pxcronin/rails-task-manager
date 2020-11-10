1c90f79c4ce6d780c63496532da9354ddadc1b8a
Patrick Cronin <pxcronin@gmail.com>
2020-11-10 14:00:29 -0500

    All CRUD functions working. Refactored to use the 'before_action' to set the @task instance where applicable. Also had the New and Edit htmls based on one form. New doesn't ask if the task is completed by checking if the @task.id exists

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,18 +18,25 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
     @task.save
   end
 
+  def destroy
+    @task.destroy
+    redirect_to index_path
+  end
+
   private
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
