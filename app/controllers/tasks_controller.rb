38339bffbf345a06f08644f1c0ccf3656d56a3ee
Patrick Cronin <pxcronin@gmail.com>
2020-11-10 13:35:53 -0500

    Edit and update are working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,7 +14,18 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to show_path(@task)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+    @task.save
   end
 
   private
